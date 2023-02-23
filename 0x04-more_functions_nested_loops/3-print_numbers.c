27e86dcf656dca45ce3eacc1e3018adbaf86a29b
NeophyteIV <marchhandrew@gmail.com>
2023-02-23 07:34:30 -0800

    Write a function that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - checks for checks for a digit (0 through 9).
+ * print_numbers - print 0123456789
  *
- * Return: Always 0.
+ * Return: void
  */
 void print_numbers(void)
 {
-       	int c;
-	for (c = 48; c < 58; c++)
-		_putchar('\n');
+	char c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
 
