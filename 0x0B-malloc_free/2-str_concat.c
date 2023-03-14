4e408b80e8f7ab6d013a5f9ebbb19836300a10ac
NeophyteIV <marchhandrew@gmail.com>
2023-03-14 02:43:50 -0700

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,18 +12,23 @@ char *str_concat(char *s1, char *s2)
 	char *conct;
 	int i, ci;
 
+
 	if (s1 == NULL)
+
 		s1 = "";
+
 	if (s2 == NULL)
+
 		s2 = "";
 
 		i = ci = 0;
+
 	while (s1[i] != '\0')
+
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
-
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
@@ -32,7 +37,6 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s1[i];
 		i++;
 	}
-
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
@@ -40,6 +44,5 @@ char *str_concat(char *s1, char *s2)
 	}
 	conct[i] = '\0';
 	return (conct);
-
 }
 
